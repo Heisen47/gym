4f33ad652f4da210363d3c10a08293d783436437
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2025-02-21 10:05:59 +0530

    add: excepetion handling

--- src/main/java/com/example/gym/service/InvoiceService.java
+++ src/main/java/com/example/gym/service/InvoiceService.java
@@ -1,5 +1,7 @@
 package com.example.gym.service;
 
+import com.example.gym.exception.InvalidInvoiceDataException;
+import com.example.gym.exception.UserNotFoundException;
 import com.example.gym.model.Invoice;
 import com.example.gym.repository.InvoiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +18,12 @@ public class InvoiceService {
     }
 
     public Invoice saveInvoice(Invoice invoice) {
+        if (invoice.getInvoiceAmount() == null ) {
+            throw new InvalidInvoiceDataException("Invoice amount and payment method are required.");
+        }
+        if (invoice.getUser() == null) {
+            throw new UserNotFoundException("User not found for the given invoice.");
+        }
         return invoiceRepository.save(invoice);
     }
 }
